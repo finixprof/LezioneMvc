9aa73033903c0ef69a28a9fdbe1884069f80b784
Salvo Finistrella <salvo.finistrella@iispascal.it>
2023-03-20 12:50:43 +0100

    aggiunto collegamento modifica foto profilo

--- Site/Models/Entities/Utente.cs
+++ Site/Models/Entities/Utente.cs
@@ -6,5 +6,7 @@
         public string Password { get; set; }
         public string Email { get; set; }
         public bool IsMailConfermata { get; set; }
+
+        public string Foto { get; set; }
     }
 }
